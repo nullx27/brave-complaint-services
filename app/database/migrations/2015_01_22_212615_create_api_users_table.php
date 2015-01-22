f21261cc2b1cc350055bdcf4279036f70d0eca35
Andre 'Necrotex' Peiffer <necrotex@gmail.com>
2015-01-22 22:57:35 +0100

    Renamed database migration files

---
+++ app/database/migrations/2015_01_22_212615_create_api_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApiUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+
+		Schema::create('api_users', function($t)
+		{
+			$t->increments('id')->unsigned();
+			$t->string('token', 64);
+			$t->text('remember_token');
+			$t->string('character_name', 128);
+			$t->integer('alliance_id');
+			$t->text('alliance_name');
+			$t->longText('user_permissions');
+			$t->text('tags');
+			$t->integer('status');
+			$t->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('api_users');
+	}
+
+}
---
+++ app/database/migrations/2015_01_22_212755_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('messages', function($table){
+			$table->increments('id');
+			$table->integer('complaint_id')->unsigned();
+			$table->integer('user_id')->unsigned();
+			$table->longText('message');
+			$table->boolean('public');
+			$table->boolean('system_msg')->nullable();
+			$table->timestamps();
+		});
+	}
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('messages');
+	}
+
+}
